523cbe890f19c2a97acfbe018e715f61b5583634
Gagnor Nohan <nohangagnor@gmail.com>
2026-06-02 22:06:47 +0200

    cv-formations

--- cv.php
+++ cv.php
@@ -19,7 +19,7 @@
                         <div class="cv-title">// Étudiant BTS SIO SLAM — 2ème année</div>
                    </div>
                    <div class="cv-contact">
-                        <span class="cv-mail"> nohan.gagnor@example.com</span>
+                        <span class="cv-mail"> nohan.gagnor@gmail.com</span>
                         <span class="cv-contact-item">07 82 23 19 79</span>
                         <span class="cv-contact-item">Dijon, 21000</span>
                         <span class="cv-contact-item">https://github.com/Noh-an</span>
@@ -27,7 +27,45 @@
                 </div>
 
                 <!-- corps du cv -->
-
+                <div class="cv-body">
+                    <!-- colonne de gauche -->
+                    <div class="cv-main">
+                        <!-- formation -->
+                        <div class="cv-section">
+                            <div class="cv-section-title">Formation</div>
+                            <div class="cv-item">
+                                <div class="cv-header">
+                                    <div class="cv-item-title"><h3>BTS Services Informatiques aux Organisations</h3></div>
+                                    <div class="cv-item-date">2025 - 2027</div>
+                                </div>
+                                <div class="cv-item-subtitle">Option SLAM - Lycée polyvalent Le Castel, Dijon</div>
+                                <div class="cv-item-description">Développement d'applications, bases de données, réseaux & sécurité, gestion de projet informatique.</div>
+                            </div>
+                            <div class="cv-item">
+                                <div class="cv-header">
+                                    <div class="cv-item-title"><h3>Dispositif en  Amont de la Qualification</h3></div>
+                                    <div class="cv-item-date">2024 - 2025</div>
+                                </div>
+                                <div class="cv-item-subtitle">Année de césure</div>
+                                <div class="cv-item-description">Recherche de formation en adéquation avec mes aspirations professionnelles.</div>
+                            </div>
+                            <div class="cv-item">
+                                <div class="cv-header">
+                                    <div class="cv-item-title"><h3>Baccalauréat STI2D</h3></div>
+                                    <div class="cv-item-date">2023 - 2024</div>
+                                </div>
+                                <div class="cv-item-subtitle">Option SIN - Lycée polyvalent Clos Maire, Beaune</div>
+                                <div class="cv-item-description">Réalisation de projets concrets mêlant l'ingénierie, l'énergie, l'informatique et l'éco-conception.</div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <!-- experience professionnelle -->
+                    <div class="cv-section">
+                        <div class="cv-section-title">Expérience professionnelle</div>  
+                        
+                    </div>
+                </div>
             </div>
         </section>
     </main>
